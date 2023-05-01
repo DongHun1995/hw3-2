64ec960fbc10dc6b90005d77883ffcc6c0ce041c
shpc136 <ch950410@gmail.com>
2023-05-01 17:47:52 +0900

    second-commit

--- matmul.c
+++ matmul.c
@@ -8,6 +8,31 @@
 #include <stdlib.h>
 
 void matmul(float *A, float *B, float *C, int M, int N, int K,
-    int threads_per_process, int mpi_rank, int mpi_world_size) {
-  // TODO: FILL_IN_HERE
+            int threads_per_process, int mpi_rank, int mpi_world_size)
+{
+  // Set the tile size
+  int tile_size = 64;
+
+// Loop over the tiles of C
+#pragma omp parallel for num_threads(threads_per_process)
+  for (int i = 0; i < M; i += tile_size)
+  {
+    for (int j = 0; j < N; j += tile_size)
+    {
+      for (int k = 0; k < K; k += tile_size)
+      {
+        // Loop over the tiles of A and B
+        for (int ii = i; ii < i + tile_size && ii < M; ii++)
+        {
+          for (int jj = j; jj < j + tile_size && jj < N; jj++)
+          {
+            for (int kk = k; kk < k + tile_size && kk < K; kk++)
+            {
+              C[ii * N + jj] += A[ii * K + kk] * B[kk * N + jj];
+            }
+          }
+        }
+      }
+    }
+  }
 }
