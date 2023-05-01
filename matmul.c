80e3fade8e0156e7ee1131e14c312ce6634f3fd6
shpc136 <ch950410@gmail.com>
2023-05-01 21:28:40 +0900

    third

--- matmul.c
+++ matmul.c
@@ -17,16 +17,16 @@ void matmul(float *A, float *B, float *C, int M, int N, int K,
 #pragma omp parallel for num_threads(threads_per_process)
   for (int i = 0; i < M; i += tile_size)
   {
-    for (int j = 0; j < N; j += tile_size)
+    for (int k = 0; k < K; k += tile_size)
     {
-      for (int k = 0; k < K; k += tile_size)
+      for (int j = 0; j < N; j += tile_size)
       {
         // Loop over the tiles of A and B
         for (int ii = i; ii < i + tile_size && ii < M; ii++)
         {
-          for (int jj = j; jj < j + tile_size && jj < N; jj++)
+          for (int kk = k; kk < k + tile_size && kk < K; kk++)
           {
-            for (int kk = k; kk < k + tile_size && kk < K; kk++)
+            for (int jj = j; jj < j + tile_size && jj < N; jj++)
             {
               C[ii * N + jj] += A[ii * K + kk] * B[kk * N + jj];
             }
